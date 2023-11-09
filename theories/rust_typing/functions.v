23258a77d5f5485ad1b3f9d7548c048b0d09e73a
Vincent Lafeychine <vincent.lafeychine@proton.me>
2023-11-09 13:46:53 +0100

    [WIP] Early typed-checked inline

--- theories/rust_typing/functions.v
+++ theories/rust_typing/functions.v
@@ -421,13 +421,21 @@ Section call.
     TypedCall π E L eκs  v (v ◁ᵥ{π} l @ function_ptr lya fp) vl tys :=
     λ T, i2p (type_call_fnptr π E L (A := A)lfts eκs l v vl tys T fp lya).
 
-  Fail Lemma type_call_inline_fnptr
-    (* NOTE: Currently added *) π r E L eκs sta ϝ lya lyv
+  Lemma type_call_inline_fnptr
+    (* NOTE: Currently added *) π r E L eκs sta
     (* NOTE: Used in RefinedC *) l v vl tys fn T:
+    (* TODO: syn types matches *)
+    (* TODO: Create ϝ for this region *)
+
+    let lya := fn.(f_args).*2 in
+    let lyv := fn.(f_local_vars).*2 in
+
+    ⌜eκs = []⌝ ∗
     (⌜Forall2 (λ ty '(_, p), ty.(ty_has_op_type) (UntypedOp p) MCNone) tys (f_args fn)⌝ ∗
       foldr (λ '(v, ty) T lsa, ∀ l, l ◁ₗ[π, Owned false] PlaceIn r @ (◁ ty) -∗ T (lsa ++ [l]))
       (λ lsa, foldr (λ ly T lsv, ∀ l, l ◁ₗ[π, Owned false] (PlaceIn ()) @ (◁ (uninit (UntypedSynType ly))) -∗ T (lsv ++ [l]))
-                    (λ lsv,
+                    (λ lsv, ∀ ϝ,
+                       ⌜∀ ϝ, elctx_sat (((λ '(_, κ, _), ϝ ⊑ₑ κ) <$> L2) ++ E) L2 ((fp κs x).(fp_elctx) ϝ)⌝ ∗
                      introduce_typed_stmt π E L ϝ fn lsa lsv lya lyv T)
                     fn.(f_local_vars).*2 [])
       (zip vl tys)
