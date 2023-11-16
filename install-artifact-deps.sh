9d24159d74d6ffdf06c8e4056d83fb4f79932f44
Lennard Gäher <gaeher@mpi-sws.org>
2023-11-16 10:04:02 +0100

    fix script

--- install-artifact-deps.sh
+++ install-artifact-deps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-opam switch create . --packages=ocaml-variants.4.14.0+options,ocaml-option-flambda
+opam switch create . --packages=ocaml-variants.4.14.0+options,ocaml-option-flambda --no-install
 eval $(opam env)
 opam repo add coq-released https://coq.inria.fr/opam/released
 opam pin add coq-stdpp stdpp/
