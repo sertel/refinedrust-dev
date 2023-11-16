36fd09b369f1db236936cac557379db3c8779833
Lennard Gäher <gaeher@mpi-sws.org>
2023-11-16 14:32:21 +0100

    fix artifact script

--- install-artifact-deps.sh
+++ install-artifact-deps.sh
@@ -3,9 +3,12 @@
 opam switch create . --packages=ocaml-variants.4.14.0+options,ocaml-option-flambda --no-install
 eval $(opam env)
 opam repo add coq-released https://coq.inria.fr/opam/released
+opam pin add coq 8.17.1
 opam pin add coq-stdpp stdpp/
+opam pin add coq-stdpp-unstable stdpp/
 opam pin add coq-iris iris/
-opam pin add coq-lambda-rust lambda-rust/
+opam pin add coq-lambda-rust.dev lambda-rust/
+make builddep
 
 cd theories/
 dune build
